1fb624abbddcd956ebafa0a1f5c0e734ab5ad59c
lena-yavorska <138155482+lena-yavorska@users.noreply.github.com>
2023-07-14 17:31:05 +0100

    Style the temperature section

--- src/index.js
+++ src/index.js
@@ -26,13 +26,19 @@ function formatDate(date) {
   
   function displayWeatherCondition(response) {
     console.log(response.data);
+  
+    let iconElement = document.querySelector("#icon");
+
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-  
+
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#description").innerHTML = response.data.weather[0].main;
-  
+
+    
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].main);
   }
   
   function searchCity(city) {
@@ -43,7 +49,7 @@ function formatDate(date) {
   }
   
   function handleSubmit(event) {
-    debugger;
+    // debugger;
     event.preventDefault();
     let city  = document.querySelector("#city-input").value;
     searchCity(city);
