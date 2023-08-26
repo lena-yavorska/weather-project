fb303ce243de8f512515a1fe1b8dba30d856701a
lena-yavorska <138155482+lena-yavorska@users.noreply.github.com>
2023-08-26 10:39:06 +0100

    Removed imperial conversion

--- src/index.js
+++ src/index.js
@@ -78,7 +78,6 @@ function formatDate(timestamp) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let descriptionElement = document.querySelector("#description");
-    let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
     let celsiusTemperature = response.data.main.temp;
@@ -88,7 +87,6 @@ function formatDate(timestamp) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
     descriptionElement.innerHTML = response.data.weather[0].description;
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
@@ -121,31 +119,6 @@ function formatDate(timestamp) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
   }
-
-  function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML  = Math.round(fahrenheitTemperature);
-  }
-
-  function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature"); 
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  }
-
-  let celsiusTemperature = null;
-
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
   let dateElement = document.querySelector("#date");
   let currentTime = new Date();
